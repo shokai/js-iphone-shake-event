68e196ab69a9ce3ccd0f617514f036e6ffc42a28
Sho Hashimoto <hashimoto@shokai.org>
2011-02-14 01:41:08 +0900

    add sample

--- iphone-shake-event.js
+++ iphone-shake-event.js
@@ -3,9 +3,9 @@ var iPhoneShake = function(){
         (function(){
             var last_shake = 0;
             var threshold = 18;
-            if(params && params.threshold) threshold = params.threshold;
-            var interval = 2000;
-            if(params && params.interval) interval = params.interval;
+            if(params && params.threshold > 0) threshold = params.threshold;
+            var interval = 2;
+            if(params && params.interval > 0) interval = params.interval;
             window.ondevicemotion = function(e) {
                 var x = e.accelerationIncludingGravity.x;
                 if(x < 0) x *= -1;
@@ -14,7 +14,7 @@ var iPhoneShake = function(){
                 var z = e.accelerationIncludingGravity.z;
                 if(z < 0) z *= -1;
                 if(x > threshold || y > threshold || z > threshold){
-                    var time =  (new Date())/1000;
+                    var time = parseInt((new Date())/1000);
                     if(last_shake + interval < time){
                         last_shake = time;
                         func(e);
